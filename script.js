accd766998c6f33f037d4eaa5ea76fee0e8a1c0c
Efe Aras <efe_aras@live.nl>
2024-01-15 12:03:38 +0100

    firstcommit

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggleBtn = document.querySelector('.nav-bars')
+const dropDownMenu = document.querySelector('.nav-dropdown')
+
+toggleBtn.onclick = function() {
+    dropDownMenu.classList.toggle('open')
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const accordionItems = document.querySelectorAll('.accordion');
+
+    accordionItems.forEach(item => {
+        item.addEventListener('click', function () {
+            this.classList.toggle('active');
+        });
+    });
+});
