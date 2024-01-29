70db5b5d528acfe48805e39ea5b58c4d71ec5c26
Olav Dekker <olavdekker2004@gmail.com>
2024-01-29 16:26:35 +0100

    testing

--- index.php
+++ index.php
@@ -1,33 +1,59 @@
-<?php
-require_once "includes/securesession.inc.php"
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GreenWear</title>
+    <title>GreenWear: clothing for you</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css_files/main.css">
-    <link rel="stylesheet" href="css_files/index.css">
+    <link rel="stylesheet" href="css_files/home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     <!-- NAVIGATION BAR -->
     <?php include "header.php"; ?>
-
-    <!-- BANNER SECTION -->
-    <h1>soon! (by end of week!)</h1>
-
     <!-- BODY SECTION -->
+    <div class="mainimage">
+        <div class="imgtxt">
+            GreenWear
+        </div>
+    </div>
+    <div class="container">
+        <div class="txtpart">
+            <div class="textbasic">
+            <h1>GreenWear</h1>
+            <h4>Discover your own style in our catalog full of different styles and clothing!!</h4>
+            </div>
+            <button>
+                <a href="catalog.php" class="noline">see more</a>
+            </button>
+        </div>
+        <div class="rightpic">
+            <img src="img/p3.jpg">
+        </div>
+    </div>
 
     
+    <div class="container">
+        <div class ="leftpic">
+            <img src="img/p1.jpg">
+        </div>
+        <div class="txtpart">
+            <div class="textbasic">
+            <h1>Create your own</h1>
+            <h4>Create perfect presents using our creator or use your own ideas to create your own personalized clothesline!!</h4>
+            </div>
+            <button>
+                <a href="design.php" class="noline">start creating!</a>
+            </button>
+        </div>
+    </div>
     <!-- FOOTER -->
     <?php include "footer.php"; ?>
 
+    <script src="script.js"></script>
 </body>
 
 </html>
