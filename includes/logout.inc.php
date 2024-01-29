33d9a9f0d0649e6b876505d736fd6d7cdfbd3d98
Efe Aras <efe_aras@live.nl>
2024-01-29 06:27:20 +0100

    .

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -2,6 +2,14 @@
 require_once "securesession.inc.php";
 require_once "databasis.inc.php";
 
+// verwijder eerst rememberme cookie
+if (isset($_COOKIE['rememberme'])) {
+    $token = $_COOKIE['rememberme'];
+    $preparedstatement = $sqliconn->prepare("DELETE FROM tokenlogin WHERE token = ?");
+    $preparedstatement->bind_param("s", $token);
+    $preparedstatement->execute();
+    setcookie('rememberme', '', time() - 3600, "/");
+}
 // log user uit
 session_unset();
 session_destroy();
