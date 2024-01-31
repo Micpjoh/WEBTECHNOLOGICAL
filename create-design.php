d3e354800c1b4d426e580b6d64f0c225c8a333fb
Efe Aras <efe_aras@live.nl>
2024-01-31 17:53:37 +0100

    .

--- create-design.php
+++ create-design.php
@@ -1,5 +1,13 @@
 <?php
 require_once "includes/securesession.inc.php";
+
+// Als user niet ingelogd is, stuur hem terug naar loginpage
+if (!isset($_SESSION['user_id'])) {
+    echo "<script>alert('You need to be logged in to create a custom piece. <br> Redirecting to login page');         
+    window.location.href = 'login.php';</script>";
+    die();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -22,98 +30,109 @@ require_once "includes/securesession.inc.php";
     <?php include "header.php"; ?>
 
     <!-- Body -->
-    <section id="body1">
-        <div class="header-text">
-            <h1>Create your piece</h1>
-        </div>
-    </section>
-    <section id="create-design-container">
-       <div class="create-design-box">
-            <div class="create-design-info">
-                <h2>Step 1: Pick your clothing piece</h2>
-                <p>
-                Our selection may be limited, but rest assured that everything you see is made of the highest quality materials.
-                Nothing in the current market comes close to our eco-friendly sourced materials ensuring that you look amazing with a piece of clothing that will last forever.
-                </p><br><br>
-            
-                <label class="button-container">Shirt
-                    <input type="radio"  name="clothing" value ="Shirt" checked>
-                    <span class="checkmark"></span>
-                </label><br>
-                <label class="button-container">Hoodie
-                    <input type="radio" name="clothing" value ="Hoodie">
-                    <span class="checkmark"></span>
-                </label><br>
-                <label class="button-container">Pants
-                    <input type="radio"  name="clothing" value ="Pants">
-                    <span class="checkmark"></span>
-                </label><br>
-                <label class="button-container">Beanie
-                    <input type="radio" name="clothing" value="Beanie">
-                    <span class="checkmark"></span>
-                </label>
+    <div class="sections-container">
+        <section id="body1">
+            <div class="header-text">
+                <h1>Create your piece</h1>
             </div>
-        </div>
-
+        </section>
+        <section id="create-design-container">
         <div class="create-design-box">
-            <div class="create-design-info">
-                <h2>Step 2: Pick your color</h2>
-                <p>
-                    We are a startup company so sadly our current selection of clothing is limited to only black and white.<br><br>
-                    But rest assured in the future we shall release different colors to pick from and many more customisation options.
-                </p><br><br><br>
-                <label class="button-container">White
-                    <input type="radio"  name="Color" value="White,with print of" checked>
-                    <span class="checkmark"></span>
-                </label><br>
-                <label class="button-container">Black
-                    <input type="radio" name="Color" value="Black,with print of">
-                    <span class="checkmark"></span>
-                </label>
-            </div>
-        </div>
-
-        <div class="create-design-box">
-            <div class="create-design-info">
-                <h2>Step 3: Create your masterpiece</h2>
-                <p>
-                    Just write whatever you wish to see on your clothing piece. Wait 20-30 seconds and be amazed by your own creation.<br><br>
-                    Don't like it? Just re-write your request to your hearts content.
-
-                    <form id="promptForm">
-                        <input type="text" id="promptInput" placeholder="Enter your prompt">
-                        <button type="submit" id ="submitButton" disabled>See your imagination come to life</button>
-                    </form>
-                </p>
-           </div>
-         <img id="resultImage" src="" alt="AI generated result" style="display:none;">
-        </div>
-    </section>
-    <button id="buyButton" class="buyButton">Buy</button>
-
-    <div id="popupWindow" class="popup" style="display: none;">
-       <div class="popup-content">
-            <span class="close-btn">&times;</span>
-            <div class="popup-body">
-                <div class="popup-image">
-                    <img id="popupImage" src="" alt="Selected Product Image">
-                </div>
-                <div class="popup-info">
-                    <h1>Your custom piece</h1><br>
-                    <p id="selectedProductDetails"></p>
-                    <p id="modelInfo"></p>
-                    <p id="colorInfo"></p>
-                    <p id="priceInfo"></p><br><br><br>
-                    <button class="payButton">Pay</button>
-                    <button class="payButton">Pay and add to catalog</button>
+                <div class="create-design-info">
+                    <h2>Step 1: Pick your clothing piece</h2>
+                    <p>
+                    Our selection may be limited, but rest assured that everything you see is made of the highest quality materials.
+                    Nothing in the current market comes close to our eco-friendly sourced materials ensuring that you look amazing with a piece of clothing that will last forever.
+                    </p><br><br>
+                
+                    <label class="button-container">Shirt
+                        <input type="radio"  name="clothing" value ="Shirt">
+                        <span class="checkmark"></span>
+                    </label><br>
+                    <label class="button-container">Hoodie
+                        <input type="radio" name="clothing" value ="Hoodie">
+                        <span class="checkmark"></span>
+                    </label><br>
+                    <label class="button-container">Pants
+                        <input type="radio"  name="clothing" value ="Pants">
+                        <span class="checkmark"></span>
+                    </label><br>
+                    <label class="button-container">Beanie
+                        <input type="radio" name="clothing" value="Beanie">
+                        <span class="checkmark"></span>
+                    </label>
                 </div>
             </div>
+
+            <div class="create-design-box">
+                <div class="create-design-info">
+                    <h2>Step 2: Pick your color</h2>
+                    <p>
+                        We are a startup company so sadly our current selection of clothing is limited to only black and white.<br><br>
+                        But rest assured in the future we shall release different colors to pick from and many more customisation options.
+                    </p><br><br><br>
+                    <label class="button-container">White
+                        <input type="radio"  name="Color" value="White with print of">
+                        <span class="checkmark"></span>
+                    </label><br>
+                    <label class="button-container">Black
+                        <input type="radio" name="Color" value="Black with print of">
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+            </div>
+
+            <div class="create-design-box">
+                <div class="create-design-info">
+                    <h2>Step 3: Create your masterpiece</h2>
+                    <p>
+                        Just write whatever you wish to see on your clothing piece. Wait 20-30 seconds and be amazed by your own creation.<br><br>
+                        Don't like it? Just re-write your request to your hearts content.
+
+                        <form id="promptForm">
+                            <input type="text" id="promptInput" placeholder="Enter your prompt">
+                            <button type="submit" id ="submitButton" disabled>See your imagination come to life</button>
+                        </form>
+                    </p>
+            </div>
+            <img id="resultImage" src="" alt="AI generated result" style="display:none;">
+            </div>
+        </section>
+
+        <section id="addtoStoreForm1">
+            <button id="buyButton" class="buyButton">Proceed to payment</button>
+        </section>
+
+        <div id="popupWindow" class="popup" style="display: none;">
+            <div class="popup-content">
+                <span class="close-btn">&times;</span>
+                <div class="popup-body">
+                    <div class="popup-image">
+                        <img id="popupImage" src="" alt="Selected Product Image">
+                    </div>
+                    <div class="popup-info">
+                        <h1>Your custom piece</h1><br>
+                        <p id="selectedProductDetails"></p>
+                        <p id="modelInfo"></p>
+                        <p id="colorInfo"></p>
+                        <p id="priceInfo"></p><br><br><br>
+                        <form action="includes/addtoStore.php" method="POST">
+                            <button class= "payButton">Pay</button>
+                        </form>
+                        <form id="addtoStoreForm" action="includes/addtoStore.php" method="POST">
+                            <input type="hidden" id="imageUrlInput" name="imageUrl" value="">
+                            <input type="hidden" id="selectedClothingInput" name="clothing" value="">
+                            <button class= "payButton">Pay and add to our catalog</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
+
     </div>
 
-
     <script src="js/create-design.js"></script>
-
+    <script src="js/addtoStore.js"></script>
     <!-- FOOTER -->
     <?php include "footer.php"; ?>
 
