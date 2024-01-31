8f5bc703d0af95b5a5564bfe5c8bae0a65ec8b1d
camila deanda <camila.de.anda@student.uva.nl>
2024-01-31 18:55:37 +0100

    .

--- product.php
+++ product.php
@@ -1,8 +1,3 @@
-<?php
-require_once "includes/securesession.inc.php";
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,7 +7,7 @@ require_once "includes/securesession.inc.php";
     <title>Catalog</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css_files/main.css">
-    <link rel="stylesheet" href="css_files/catalog.css">
+    <link rel="stylesheet" href="css_files/product.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="js/catalog.js"></script>
 </head>
@@ -26,64 +21,76 @@ require_once "includes/securesession.inc.php";
         <div id="header-cloth">
             <h1>Our products</h1>
             <h2>upgrade your wardrobe with your favorite prints on our high quality basics</h2>
-                         
         </div>
     </section>
 
     <section id='content'>
-        <div id='cloth-container'>
-            <div class='cloth-rows'>
-            <?php
-                require_once "includes/databasis.inc.php";
+        <div id='product-container'>
+        
+    <?php
 
-                if(isset($_GET['product_id'])){
-                    // Sanitize input to prevent SQL injection
-                    $product_id = $sqliconn->real_escape_string($_GET['product_id']);
+    require_once "includes/databasis.inc.php";
+    require_once "includes/securesession.inc.php";
 
-                    // Query for products with stock greater than 0
-                    $sql = "SELECT * FROM products WHERE product_id = $product_id";
-                    $result = $sqliconn->query($sql);
+    if(isset($_GET['product_id'])){
+                   // Sanitize input to prevent SQL injection
+        $product_id = $sqliconn->real_escape_string($_GET['product_id']);
 
-                    // Error handling
-                    if (!$result) {
-                        die('Error fetching products: ' . $sqliconn->error);
-                    }
+        // Query for products with stock greater than 0
+        $sql = "SELECT * FROM products WHERE product_id = ?";
+        $secure = $sqliconn->prepare($sql);
 
-                    while($row = $result->fetch_assoc()){
-                        $product_name = $row['product_name'];
-                        $product_img = $row['img'];
-                        $price = $row['price'];
+        $secure->bind_param("i", $product_id);
 
-                        echo "
-                        <div class='cloth'>
-                            <div class='img-here'>
-                                <img src='$product_img'>
-                            </div>
-                            <div class='name-container'>
-                                <h2 class='name'>$product_name</h2>
-                            </div>
-                        </div>";
-                    }
-                }
+        $secure->execute();
 
-                if(isset($_GET['product_id'])){
-                    // Sanitize input to prevent SQL injection
-                    $product_id = $sqliconn->real_escape_string($_GET['product_id']);
+        $result = $secure->get_result();
 
-                    // Query for products with stock greater than 0
-                    $sql = "SELECT * FROM products WHERE product_id = $product_id";
-                    $result = $sqliconn->query($sql);
 
-                    // Error handling
-                    if (!$result) {
-                        die('Error fetching products: ' . $sqliconn->error);
-                    }
-            ?>
+        // Error handling
+        if (!$result) {
+            die('Error fetching products: ' . $secure->error);
+        }
+
+        while($row = $result->fetch_assoc()){
+            $product_name = $row['product_name'];
+            $product_img = $row['img'];
+            $price = $row['price'];
+            $desc = $row['product_info'];
+            $stock = $row['stock'];
+
+                    
+                       
+
+
+            echo "
+        <div class='product-left'>
+            <div class='img-here'>
+                <img src='$product_img'>
             </div>
-            </div>        
+        </div>
+        <div class='product-right'>
+            <h2 class='name'>$product_name</h2>
+            <h3 class='desc'>$desc</h3>
+            <form action = 'includes/addtocart.php' method = 'post'>
+                <input type = 'hidden' name = 'product_id' value = '$product_id'>
+                <input type='number' value ='1' min = '1' max ='$stock' id='input-quan' name = 'quantity'>
+                <input type='submit' name= 'add-cart'>
+            </form>    
+        </div>";
+            };      
+      
+        };
 
 
-</section>"
+ 
+        ?>
+
+
+        </div>        
+
+
+    </section>
 
    
     <!-- FOOTER -->
@@ -91,5 +98,4 @@ require_once "includes/securesession.inc.php";
     <?php include "footer.php"; ?>
     
 </body>
-
 </html>
