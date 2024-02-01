cdb760c45bbd7960ed3b2768794174dca940235d
Efe Aras <efe_aras@live.nl>
2024-02-01 11:25:27 +0100

    old files in

--- js/addtoStore.js
+++ js/addtoStore.js
@@ -1,18 +1,13 @@
-// select all radio buttons, check for change, if yes, make it the value
 document.querySelectorAll('label.button-container input[name="clothing"]').forEach(function(radioButton) {
     radioButton.addEventListener('change', function() {
         var selectedClothing = this.value;
+        console.log("Selected Clothing:", selectedClothing);
 
         // Update the hidden input field with the selected clothing value
         document.getElementById('selectedClothingInput').value = selectedClothing;
     });
 });
-<<<<<<< HEAD
-//Turn bloburl to base64 url to display in the admin 
-=======
 
-// Use to change a blob file in a base64URL, which we then can use as img in database
->>>>>>> a347955 (.)
 function blobUrlToBase64(blobUrl, callback) {
     const xhr = new XMLHttpRequest();
     xhr.onload = function() {
@@ -26,7 +21,7 @@ function blobUrlToBase64(blobUrl, callback) {
     xhr.responseType = 'blob';
     xhr.send();
 }
-//add image to store
+
 document.getElementById('addtoStoreForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -39,4 +34,3 @@ document.getElementById('addtoStoreForm').addEventListener('submit', function(ev
     });
 });
 
-//all the code here prepares the images to be transported to the catalog and admin page
