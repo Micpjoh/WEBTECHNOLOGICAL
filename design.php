072113ea119581784937a45a43dce769ef07ef3b
Olav Dekker <olavdekker2004@gmail.com>
2024-02-01 20:00:57 +0100

    design.php

--- design.php
+++ design.php
@@ -8,7 +8,7 @@ require_once "includes/securesession.inc.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
+    <title>Design</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css_files/main.css">
     <link rel="stylesheet" href="css_files/design.css">
